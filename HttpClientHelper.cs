31ce0b1f405718df038e2f09d3e5ef39cda16884
Bryanlhx <bryan.lhx@gmail.com>
2018-09-09 22:37:49 +0800

    Add files via upload

---
+++ HttpClientHelper.cs
@@ -0,0 +1,137 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SymphonyAdapter.CommonFunction
+{
+    public class HttpClientHelper
+    {
+        /// <summary>
+        /// get
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static string GetResponse(string url, out string statusCode)
+        {
+            if (url.StartsWith("https"))
+                System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Add(
+              new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = httpClient.GetAsync(url).Result;
+            statusCode = response.StatusCode.ToString();
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                return result;
+            }
+            return null;
+        }
+
+        public static string RestfulGet(string url)
+        {
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            // Get response
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            {
+                // Get the response stream
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                // Console application output
+                return reader.ReadToEnd();
+            }
+        }
+
+        public static T GetResponse<T>(string url)
+           where T : class, new()
+        {
+            if (url.StartsWith("https"))
+                System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Add(
+               new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = httpClient.GetAsync(url).Result;
+
+            T result = default(T);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Task<string> t = response.Content.ReadAsStringAsync();
+                string s = t.Result;
+
+                result = JsonConvert.DeserializeObject<T>(s);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// post
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="postData">post data</param>
+        /// <returns></returns>
+        public static string PostResponse(string url, string postData, out string statusCode)
+        {
+            if (url.StartsWith("https"))
+                System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+
+            HttpContent httpContent = new StringContent(postData);
+            httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            httpContent.Headers.ContentType.CharSet = "utf-8";
+
+            HttpClient httpClient = new HttpClient();
+            //httpClient..setParameter(HttpMethodParams.HTTP_CONTENT_CHARSET, "utf-8");
+
+            HttpResponseMessage response = httpClient.PostAsync(url, httpContent).Result;
+
+            statusCode = response.StatusCode.ToString();
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                return result;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// post response
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="url">url</param>
+        /// <param name="postData">postData</param>
+        /// <returns></returns>
+        public static T PostResponse<T>(string url, string postData)
+            where T : class, new()
+        {
+            if (url.StartsWith("https"))
+                System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+
+            HttpContent httpContent = new StringContent(postData);
+            httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpClient httpClient = new HttpClient();
+
+            T result = default(T);
+
+            HttpResponseMessage response = httpClient.PostAsync(url, httpContent).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                Task<string> t = response.Content.ReadAsStringAsync();
+                string s = t.Result;
+
+                result = JsonConvert.DeserializeObject<T>(s);
+            }
+            return result;
+        }
+    }
+}
